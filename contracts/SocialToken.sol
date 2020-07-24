3a4775d1fadd95e178c95f7c8c50048c9327157d
dmcclung <dylanmcclung@gmail.com>
2020-07-23 19:58:56 -0500

    Adding version function

--- contracts/SocialToken.sol
+++ contracts/SocialToken.sol
@@ -4,8 +4,18 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SocialToken is ERC20 {
+
+    string private _version = "1.0";
+
     constructor(uint256 initialSupply) public ERC20("Social", "SCL") {
         _mint(msg.sender, initialSupply);
+    }
+
+    /**
+     * @dev Returns the version of the token.
+     */
+    function version() public view returns (string memory) {
+        return _version;
     }
 
 }
