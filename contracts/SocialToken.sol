a37ac820cc51724362d2fbf288f7c07de4de76d7
dmcclung <dylanmcclung@gmail.com>
2020-07-23 19:30:44 -0500

    Importing ERC20 from openzeppelin

--- contracts/SocialToken.sol
+++ contracts/SocialToken.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.6.0;
 
-contract SocialToken {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SocialToken is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("Social", "SCL") {
+        _mint(msg.sender, initialSupply);
+    }
 
 }
 
