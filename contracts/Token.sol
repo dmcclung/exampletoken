043ddea02060cbe23a4b645f68faf59dd5138ed2
dmcclung <dylanmcclung@gmail.com>
2020-07-30 17:13:52 -0500

    Note quite done and more to come, but needed to commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,33 +1,112 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.6.0;
+pragma solidity >=0.6.0 <0.7.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract Token is ERC20 {
-
+/**
+ * This token incorporates a cap and crowdsale components along with a faucet capability for users
+ * that are unable to participate in the crowdsale.
+ */
+contract Token is ERC20Capped {
     string private _version = "1.0";
 
-    constructor() public ERC20("ExampleToken", "EXT") {
-        // initial supply is 50mil reserved to owner
-        // 10**4 is reserved for small give aways
-        // tokenCap is 150mil
+    uint256 private _reservedSupply = 50 * 10**6 * 10**18;
+    uint256 private _faucetSupply = 1 * 10**4 * 10**18;
+    uint256 private _tokenCap = 150 * 10**6 * 10**18;
+    uint256 private _exchangeRate = 10000;
 
-        // exchange rate is 10000 SCL to 1 eth
+    string private _tokenName = "ExampleToken";
+    string private _tokenSymbol = "EXT";
 
-        // reserve some for myself for future purposes
+    // Token sale has a start and end date, final means that the sale has ended and
+    // the ETH collected has been sent to the contract owner's configured address
+    bool private _isFinal;
+    uint256 private _tokenSaleStartBlock;
+    uint256 private _tokenSaleEndBlock;
+    address payable private _ethAddress;
 
-        // implement a function that gives away a small fraction of 
-        // token for users that can't fund, like 10**4
-        
-        _mint(msg.sender, 5 * 10**6);
+    address private _reservedSupplyAddress;
+    address private _faucetSupplyAddress;
+
+    event CreateEXT(address indexed _to, uint256 _value);
+
+    constructor(uint256 tokenSaleStartBlock, uint256 tokenSaleEndBlock, address payable ethAddress,
+        address reservedSupplyAddress, address faucetSupplyAddress) public
+        ERC20(_tokenName, _tokenSymbol) ERC20Capped(_tokenCap)  {
+        _isFinal = false;
+        _tokenSaleStartBlock = tokenSaleStartBlock;
+        _tokenSaleEndBlock = tokenSaleEndBlock;
+        _ethAddress = ethAddress;
+        _reservedSupplyAddress = reservedSupplyAddress;
+        _faucetSupplyAddress = faucetSupplyAddress;
+
+        _mint(_reservedSupplyAddress, _reservedSupply);
+        emit CreateEXT(_reservedSupplyAddress, _reservedSupply);
+
+        _mint(_faucetSupplyAddress, _faucetSupply);
+        emit CreateEXT(_faucetSupplyAddress, _faucetSupply);
+    }
+
+    modifier isOpen() {
+        require(!_isFinal, "Token sale has been finalized");
+        require(block.number >= _tokenSaleStartBlock, "Token sale has not started");
+        _;
     }
 
     /**
-     * 
+     * @dev Create new tokens given the ETH sent and the exchange rate.
      */
+    function createTokens() external isOpen payable {
+        require(block.number < _tokenSaleEndBlock, "Token sale has ended");
+        require(msg.value > 0, "Non-zero ETH required to create tokens");
+
+        uint256 tokens = SafeMath.mul(msg.value, _exchangeRate);
+        _mint(msg.sender, tokens);
+        emit CreateEXT(msg.sender, tokens);
+    }
 
     /**
-     * @dev Returns the version of the token.
+     * @dev Sends collected eth to configured address
+     */
+    function finalizeSale() external isOpen {
+        // cannot finalize until end block
+        require(block.number >= _tokenSaleEndBlock, "Token sale has not ended");
+
+        _isFinal = true;
+        _ethAddress.transfer(address(this).balance);
+    }
+
+    /**
+     * @dev Returns the block number marking the end of the token sale
+     */
+    function tokenSaleEndBlock() public view returns (uint256) {
+        return _tokenSaleEndBlock;
+    }
+
+    /**
+     * @dev Returns the block number marking the start of the token sale
+     */
+    function tokenSaleStartBlock() public view returns (uint256) {
+        return _tokenSaleStartBlock;
+    }
+
+    /**
+     * @dev Returns true if the token sale has been finalized
+     */
+    function isFinal() public view returns (bool) {
+        return _isFinal;
+    }
+
+    /**
+     * @dev Returns the exchange rate of EXT to 1 ETH
+     */
+    function exchangeRate() public view returns (uint256) {
+        return _exchangeRate;
+    }
+
+    /**
+     * @dev Returns the version of the token
      */
     function version() public view returns (string memory) {
         return _version;
--- test/token.js
+++ test/token.js
@@ -1,10 +1,50 @@
 const Token = artifacts.require("Token");
 
 contract("The Token contract", async accounts => {
-    it("should return the version", async () => {
-        const contract = await Token.deployed();
 
-        const version = await contract.version();
-        assert.equal("1.0", version);
+    let contract;
+
+    beforeEach(async () => {
+        contract = await Token.new(2, 5);
     });
+
+    it("should return the version", async () => {
+        const version = await contract.version();
+        assert.equal(version, "1.0");
+    });
+
+    it("should return the token sale start block", async () => {
+        const tokenSaleStartBlock = await contract.tokenSaleStartBlock();
+        assert.equal(tokenSaleStartBlock, 2);
+    });
+
+    it("should return the token sale end block", async () => {
+        const tokenSaleEndBlock = await contract.tokenSaleEndBlock();
+        assert.equal(tokenSaleEndBlock, 5);
+    });
+
+    it("should not close the sale if already closed", async () => {
+        assert.fail("Not implemented yet");
+    });
+
+    it("should allocate tokens to purchases equal to the exchange rate", async () => {
+        assert.fail("Not implemented yet");
+    });
+
+    it("should not allow payable to a fallback or receive method", async () => {
+        assert.fail("Not implemented yet");
+    });
+
+    it("should return the balance allocated", async () => {
+        assert.fail("Not implmemented yet");
+    });
+
+    it("should return the cap", async () => {
+        const cap = await contract.cap();
+        assert.equal(cap, 150 * 10**6 * 10**18);
+    });
+    
+    it("should reject token requests over the cap", async () => {        
+        assert.fail("Not implemented yet");
+    });    
 });
